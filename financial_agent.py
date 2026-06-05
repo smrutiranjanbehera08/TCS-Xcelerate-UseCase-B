eb7b00936715fc9cb75082e2bd37aec1766c983f
smrutiranjanbehera08 <smrutiranjan0405@gmail.com>
2026-06-06 00:39:35 +0530

    Added dynamic stock symbol support

--- financial_agent.py
+++ financial_agent.py
@@ -1,9 +1,17 @@
 import yfinance as yf
 
-stock = yf.Ticker("RELIANCE.NS")
+symbol = input("Enter Stock Symbol: ")
 
-info = stock.info
+ticker = yf.Ticker(symbol)
 
+info = ticker.info
+
+print("\n===== COMPANY REPORT =====")
 print("Company:", info.get("longName"))
+print("Current Price:", info.get("currentPrice"))
 print("Market Cap:", info.get("marketCap"))
 print("PE Ratio:", info.get("trailingPE"))
+print("Sector:", info.get("sector"))
+print("Industry:", info.get("industry"))
+print("52 Week High:", info.get("fiftyTwoWeekHigh"))
+print("52 Week Low:", info.get("fiftyTwoWeekLow"))
