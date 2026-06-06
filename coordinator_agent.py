bb764ce890de7c60e6ae8d0f9e16e21127593a66
smrutiranjanbehera08 <smrutiranjan0405@gmail.com>
2026-06-06 23:42:48 +0530

    Added Report Writer Agent with file export

--- coordinator_agent.py
+++ coordinator_agent.py
@@ -2,33 +2,46 @@ from financial_agent import get_financial_data
 from news_agent import get_news
 from peer_comparison_agent import compare_companies
 
-symbol = input("Enter Stock Symbol: ")
+def get_complete_report(symbol):
 
-financial_data = get_financial_data(symbol)
+    financial_data = get_financial_data(symbol)
 
-news_data = get_news(symbol)
+    news_data = get_news(symbol)
 
-peer_data = compare_companies([
-    "TCS.NS",
-    "INFY.NS",
-    "WIPRO.NS"
-])
+    peer_data = compare_companies([
+        "TCS.NS",
+        "INFY.NS",
+        "WIPRO.NS"
+    ])
 
-print("\n===== COMPANY REPORT =====\n")
+    return {
+        "financial": financial_data,
+        "news": news_data,
+        "peer": peer_data
+    }
 
-for key, value in financial_data.items():
-    print(f"{key}: {value}")
 
-print("\n===== LATEST NEWS =====\n")
+if __name__ == "__main__":
 
-for i, headline in enumerate(news_data, start=1):
-    print(f"{i}. {headline}")
+    symbol = input("Enter Stock Symbol: ")
 
-print("\n===== PEER COMPARISON =====\n")
+    report = get_complete_report(symbol)
 
-for company in peer_data:
-    print("Company:", company["Company"])
-    print("Current Price:", company["Current Price"])
-    print("Market Cap:", company["Market Cap"])
-    print("PE Ratio:", company["PE Ratio"])
-    print("-" * 40)
+    print("\n===== COMPANY REPORT =====\n")
+
+    for key, value in report["financial"].items():
+        print(f"{key}: {value}")
+
+    print("\n===== LATEST NEWS =====\n")
+
+    for i, headline in enumerate(report["news"], start=1):
+        print(f"{i}. {headline}")
+
+    print("\n===== PEER COMPARISON =====\n")
+
+    for company in report["peer"]:
+        print("Company:", company["Company"])
+        print("Current Price:", company["Current Price"])
+        print("Market Cap:", company["Market Cap"])
+        print("PE Ratio:", company["PE Ratio"])
+        print("-" * 40)
