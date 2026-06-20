c679c229ba0beaf1e6e8aa5f4c00cbdddeda2812
smrutiranjanbehera08 <smrutiranjan0405@gmail.com>
2026-06-20 19:35:52 +0530

    Updated Gemini API configuration for Streamlit Cloud deployment

--- ai_thesis_agent.py
+++ ai_thesis_agent.py
@@ -1,10 +1,14 @@
 import google.generativeai as genai
 from dotenv import load_dotenv
+import streamlit as st
 import os
 
 load_dotenv()
 
-api_key = os.getenv("GEMINI_API_KEY")
+try:
+    api_key = st.secrets["GEMINI_API_KEY"]
+except:
+    api_key = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=api_key)
 
